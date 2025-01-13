abf3a9207bd899de4f3b5e4e9c1665587696ce8e
Diego-Moretti <diegocarmino@hotmail.com>
2025-01-13 14:17:44 -0300

    aula 2 tipos variaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudar a classe string que representa textos na aplicacao
+        
+        double salarioMinino = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = numeroCurto;
+        
+        final double VALOR_DE_PI = 3.14;
     }
 }
