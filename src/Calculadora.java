ae7d6dc60f0374038a5c191c577ae146d6311ae8
Diego-Moretti <diegocarmino@hotmail.com>
2025-01-20 18:20:11 -0300

    aula sintaxe finalizada

---
+++ src/Calculadora.java
@@ -0,0 +1,26 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matematicas entre numeros
+ * <p>
+ * <b>Note:</b> Leia atentamente  a documentação destas classes
+ * para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Diego Moretti
+ * @version 1.0
+ * @since   15/01/2024
+ */
+public class Calculadora {
+    /** 
+    * Este metodo é utilizado para somar dois numeros inteiros
+    * @param numeroUm este é o primeiro parametro do metodo
+    * @param numeroDois este é o segundo parametro do metodo
+    * @return int o resultado deste método é a soma dos dois numeros. 
+    */
+    
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;  
+    }
+
+
+
+}
